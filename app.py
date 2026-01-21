a8a4dd8caff984a40b1947811ce68527a9c81516
wata1558 <wnori1558@gmail.com>
2026-01-22 08:54:43 +0900

    update

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ app = FastAPI(title="Static Image YOLO Detection")
 
 model = YOLO("best32.pt")  # Render にモデルがあることを確認
 
-model.model = quantize_dynamic(
-    model.model,
-    {torch.nn.Linear, torch.nn.Conv2d},
-    dtype=torch.qint8
-)
+#model.model = quantize_dynamic(
+    #model.model,
+    #{torch.nn.Linear, torch.nn.Conv2d},
+    #dtype=torch.qint8
+#)
 
 label_map = {
     "can": "缶",
